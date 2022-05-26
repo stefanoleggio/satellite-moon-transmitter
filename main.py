80454d4d72b25e283c81d4d3616382dd0d6d895f
stefano28 <stefanoleggio28@gmail.com>
2022-05-26 21:45:07 +0200

    adding doppler effect and output quantizer

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-import random
+import os
 
 def signalPlot(data, T, total_duration, title):
     t = np.arange(0, total_duration, T)
@@ -11,6 +11,17 @@ def signalPlot(data, T, total_duration, title):
     plt.title(title)
     plt.show()
 
+
+def quantizer(value):
+
+    quantized_value = abs(round(value/V_FS * (pow(2,BIT_FOR_IQ-1)-1)))
+    if(abs(value) > V_FS):
+        quantized_value = pow(2,BIT_FOR_IQ - 1) - 1
+    
+    if(value<0):
+        quantized_value = ~quantized_value + pow(2,BIT_FOR_IQ-1) + 1
+    return quantized_value
+
  # Usage example:   python main.py
  #                  python main.py -p (for plotting)
 
@@ -18,8 +29,12 @@ if __name__ == '__main__':
 
     chunk_len = 10 # Bits for chunk - TODO: symbol rate as a variable multiple of the PRN duration
 
+    if(os.path.exists("output.bin")):
+        os.remove("output.bin")
+
     message = open("message.bin", "rb")
     codes = json.load(open("codes.json", "r"))
+    output_file = open("output.bin","a")
 
     PRN_SEQUENCE = codes['prn_sequence']
     PRN_SEQUENCE_INVERSE = codes['prn_sequence_inverse']
@@ -31,6 +46,10 @@ if __name__ == '__main__':
     TEMPERATURE = 300
     BAND = 1.023 * pow(10,6)
     N_0 = BOLTZMANN_COSTANT*TEMPERATURE
+    F_S = 100 # Sampling frequency
+
+    V_FS = pow(10,-4)
+    BIT_FOR_IQ = 16
 
     bit_count = 0
     chunk_count = 0
@@ -99,20 +118,29 @@ if __name__ == '__main__':
 
             boc_for_message_bit_size = len(boc_values_message_bit)
 
-            awgn_vector = (np.random.randn(boc_for_message_bit_size) + 1j*np.random.randn(boc_for_message_bit_size)) * np.sqrt(N_0*BAND/2)
-            path_loss_vector = np.random.uniform(0,pow(10, -8),boc_for_message_bit_size)
-                     
-            boc_values_message_bit_channel = boc_values_message_bit * path_loss_vector + awgn_vector
 
-            """
-            plt.plot(np.real(boc_values_message_bit_channel),np.imag(boc_values_message_bit_channel), '.')
-            plt.grid()
-            plt.show()
-            """
+            # Parameters for simulation
+
+            awgn_vector = (np.random.randn(boc_for_message_bit_size) + 1j*np.random.randn(boc_for_message_bit_size)) * np.sqrt(N_0*BAND/2)
+            path_loss_vector = np.random.uniform(pow(10, -8),pow(10,-5),boc_for_message_bit_size)
+            doppler_shift_vector = np.random.uniform(2000,5000,boc_for_message_bit_size)
+            latency_vector = np.random.uniform(5,10,boc_for_message_bit_size)*pow(10,-6)
+
+            boc_values_message_bit_channel = boc_values_message_bit * path_loss_vector * np.exp(1j*2*np.pi*doppler_shift_vector*latency_vector) + awgn_vector
+
+            for boc_value_message_bit_channel in boc_values_message_bit_channel:
+                real_sample = quantizer(np.real(boc_value_message_bit_channel))
+                imag_sample = quantizer(np.imag(boc_value_message_bit_channel))
+                output_file.write(bin(real_sample)[2:].zfill(16))
+                output_file.write(bin(imag_sample)[2:].zfill(16))
+
+            #plt.plot(np.real(boc_values_message_bit_channel),np.imag(boc_values_message_bit_channel), '.')
+            #plt.grid()
+            #plt.show()
 
             chunk_channel.append(boc_values_message_bit_channel)
 
-        print("Chunk with channel effects: AWGN and PATH LOSS")
+        print("Chunk with channel effects: AWGN, PATH LOSS and DOPPLER SHIFT")
         print(chunk_channel)
         print("\n-------------------------------------------------\n")
 
