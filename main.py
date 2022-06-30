33e184f64cf94a99babe2c401cb43e12a473d9e7
stefano28 <stefanoleggio28@gmail.com>
2022-06-30 16:47:24 +0200

    quantizator implementation

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def iqPlot(values,title):
     plt.close()
 
 
+def quantize_uniform(x, quant_min=-1.0, quant_max=1.0, quant_level=5):
+
+    x_normalize = (x-quant_min) * (quant_level-1) / (quant_max-quant_min)
+    x_normalize[x_normalize > quant_level - 1] = quant_level - 1
+    x_normalize[x_normalize < 0] = 0
+    x_normalize_quant = np.around(x_normalize)-pow(2,BIT_FOR_IQ-1)
+
+    return x_normalize_quant
+
 def quantizer(value):
 
     quantized_value = abs(round(value/V_FS * (pow(2,BIT_FOR_IQ-1)-1)))
@@ -47,7 +56,8 @@ if __name__ == '__main__':
     TEMPERATURE = 300
     BAND = 1.023 * pow(10,6)
     N_0 = BOLTZMANN_COSTANT*TEMPERATURE
-    F_S = 100 # Sampling frequency
+    F_S = 4*pow(10,6) # Sampling frequency
+
 
     V_FS = pow(10,-4)
     BIT_FOR_IQ = 16
@@ -131,38 +141,49 @@ if __name__ == '__main__':
             doppler_shift_vector = np.random.uniform(2000,5000,boc_for_message_bit_size)
             latency_vector = np.random.uniform(5,10,boc_for_message_bit_size)*pow(10,-6)
 
+            # Plot the BOC(1,1)
             if(firstPlot and len(sys.argv)>1 and sys.argv[1] == "-p"):
                 iqPlot(boc_values_message_bit, "BOC(1,1) output")
 
+            # Doppler shift
             boc_values_message_bit_channel = boc_values_message_bit * np.exp(1j*2*np.pi*doppler_shift_vector*latency_vector)
 
             if(firstPlot and len(sys.argv)>1 and sys.argv[1] == "-p"):
                 iqPlot(boc_values_message_bit_channel, "IQ samples with doppler shift")
 
             if(noiseFlag):
+                # AWGN
                 boc_values_message_bit_channel = boc_values_message_bit_channel + awgn_vector
                 if(firstPlot and len(sys.argv)>1 and sys.argv[1] == "-p"):
                     iqPlot(boc_values_message_bit_channel, "IQ samples with AWGN")
 
+            # Path loss
             boc_values_message_bit_channel = boc_values_message_bit_channel * path_loss_vector
+
+            for i in range(len(boc_values_message_bit_channel)):
+                boc_values_message_bit_channel[i] = F_S * 1/(2*BAND+doppler_shift_vector[i])
 
             if(firstPlot and len(sys.argv)>1 and sys.argv[1] == "-p"):
                 iqPlot(boc_values_message_bit_channel, "IQ samples with path loss")
 
-
             for boc_value_message_bit_channel in boc_values_message_bit_channel:
-                real_sample = quantizer(np.real(boc_value_message_bit_channel))
-                imag_sample = quantizer(np.imag(boc_value_message_bit_channel))
+
+                real_sample = int(quantize_uniform(np.array([np.real(boc_value_message_bit_channel)]), -V_FS, V_FS,pow(2,BIT_FOR_IQ))[0])
+                imag_sample = int(quantize_uniform(np.imag([np.real(boc_value_message_bit_channel)]), -V_FS, V_FS,pow(2,BIT_FOR_IQ))[0])
                 output_file.write(real_sample.to_bytes(2,byteorder='big',signed=True))
                 output_file.write(imag_sample.to_bytes(2,byteorder='big',signed=True))
+                output_file.flush()
 
             chunk_channel.append(boc_values_message_bit_channel)
 
             firstPlot = False
 
-        print("Chunk with channel effects: AWGN, PATH LOSS and DOPPLER SHIFT")
-        print(chunk_channel)
+        print("Chunk with channel effects: AWGN (if flag True), PATH LOSS and DOPPLER SHIFT")
+        print(str(chunk_channel[0]) + "...")
         print("\n-------------------------------------------------\n")
         
         chunk_count += 1
         bit_count = 0
+
+    message.close()
+    output_file.close()
